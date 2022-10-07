420702ad84e76b822fcacf46d48e10ff832dd6ba
isidoraconic <43223777+isidoraconic@users.noreply.github.com>
2022-10-07 09:31:32 -0700

    Add files via upload

---
+++ naive-bayes.md
@@ -0,0 +1,74 @@
+# Naive-Bayes Assignment
+
+#### Isidora Conic - Machine Learning (CS 6140)
+
+For this assignment, I first implemented my Naive-Bayes model, and then further tried to optimize it by removing stop-words (see below). 
+
+## (1) Naive-Bayes Classifier
+
+The first iteration of my classifier was as outlined in class. I further optimized the hyperparameters, `v` (total vocabulary words; total number of words in the english language) as well as `alpha` (used for smoothing). I found that the best values for these paramters were as follows:
+
+​	`v = 170000`  
+
+​	`alpha = 0.005` 
+
+### Results/Metrics: 
+
+I set `P` (condition positive) as the SPAM emails, and `R` (condition negative) as the HAM emails. The results for this model (classification on the 100 emails in the test set) were as follows:
+
+`P = 37`
+
+`R = 63`
+
+`TP (SPAM classified as SPAM) = 36`
+
+`FN (SPAM classified as HAM) =  1`
+
+`FP (HAM classified as SPAM) =  13`
+
+`TN (HAM classified as HAM) =  50`
+
+`f-score =  0.8372093023255814`
+
+`precision =  0.7346938775510204`
+
+`recall =  0.972972972972973`
+
+## (2) Naive-Bayes Classifier with removal of Stop-Words
+
+In this second attempt, I removed stop-words (as defined in [Ranks NL](https://www.ranks.nl/stopwords)). In this case, when creating the dictionairies for my model, if a word came up that was in this list, I didn't include it. Similarly, when classifying a word/email, if the word was in the list of stop words (found in the `stop-words.txt` file), I did not include it in the probability calculation.
+
+Contrary to what I anticipated, this did not improve the performance of my classifier. Initially, I believed that it would reduce noise that would come from using words that don't hold any semantic importance as part of the classification (in in turn improve classification). I speculate that the performance of this classifier might have been worse due to a reduced size of training set (now that stop-words are removed), and that perhaps in this case, these stop-words did in fact hold some importance. Further, seeing as I used a pre-defined set of stop-words (not onces that I myself chose), some of these words may have actually been relevant in the classification.
+
+The same hyperparameters as above were used.
+
+### Results/Metrics:
+
+Same as in the previous classifier, I set `P` (condition positive) as the SPAM emails, and `R` (condition negative) as the HAM emails. The results for this model without stop-words (classification on the 100 emails in the test set) were as follows:
+
+`P = 37`
+
+`R = 63`
+
+`TP (SPAM classified as SPAM) = 35`
+
+`FN (SPAM classified as HAM) =  2`
+
+`FP (HAM classified as SPAM) =  13`
+
+`TN (HAM classified as HAM) =  50`
+
+`f-score =  0.8235294117647058`
+
+`precision =  0.7291666666666666`
+
+`recall =  0.9459459459459459`
+
+
+
+
+
+
+
+
+
---
+++ naive-bayes/naive-bayes-stop-words.py
@@ -0,0 +1,200 @@
+import os
+import math
+
+ham_dict = dict()
+spam_dict = dict()
+
+# get list of all the files in spam and ham, respectively
+ham_email_files = os.listdir("../HamSpam/ham")
+spam_email_files = os.listdir("../HamSpam/spam")
+
+# get list of stop words from stop words file, and dont use those in training
+stop_words = list()
+with open("../HamSpam/stop-words.txt", 'r') as sw:
+    stop_words = sw.readlines()
+# removing all new lines from the words
+stop_words = list(map(lambda s: s.strip(), stop_words))
+
+# getting total number of emails in each category (ham vs. spam)
+total_ham_email_count = len(ham_email_files)
+total_email_spam_count = len(spam_email_files)
+
+# creating basic dictionaries with just the word counts (no log, smoothing, or probabilities)
+# creating the ham dictionary
+for ham_filename in ham_email_files:
+    with open(os.path.join("../HamSpam/ham", ham_filename), 'r') as h:
+        ham_file_lines = h.readlines()
+        for ham_line in ham_file_lines:
+            ham_line = ham_line.strip()
+            if ham_line.lower() in stop_words:
+                continue
+            elif ham_line in stop_words:
+                continue
+            elif ham_line in ham_dict:
+                ham_dict[ham_line] = ham_dict[ham_line] + 1
+            else:
+                ham_dict[ham_line] = 1
+# creating the spam dictionary
+for spam_filename in spam_email_files:
+    with open(os.path.join("../HamSpam/spam", spam_filename), 'r') as s:
+        spam_file_lines = s.readlines()
+        for spam_line in spam_file_lines:
+            spam_line = spam_line.strip()
+            if spam_line.lower() in stop_words:
+                continue
+            elif spam_line in stop_words:
+                continue
+            elif spam_line in spam_dict:
+                spam_dict[spam_line] = spam_dict[spam_line] + 1
+            else:
+                spam_dict[spam_line] = 1
+
+# wordcount of unique words in ham and spam training sets
+ham_train_wordcount = len(ham_dict)
+spam_train_wordcount = len(spam_dict)
+
+# n is the number of unique words in our training sets (both ham and spam) --> is this right?
+n = ham_train_wordcount + spam_train_wordcount
+
+# n_spam is the total number of words in the spam emails (not unique)
+n_spam = 0
+for value in spam_dict.values():
+    n_spam = n_spam + value
+
+# n_ham is the total number of words in the ham emails (not unique)
+n_ham = 0
+for value in ham_dict.values():
+    n_ham = n_ham + value
+
+# hyperparams for smoothing
+alpha = 0.005
+
+# v is the total words in the english language, this is an estimate
+v = 170000
+
+# converting the dictionary entries into probabilities, and take the log of each
+# note that these are SEEN words (that we got from the training set)
+# the formula for prob P(word|HAM) or P(word|SPAM) = word_count/(n+v) where n is n_ham or n_spam
+# ham probability (log) dictionary first, making an implicit copy
+ham_log_prob = ham_dict.copy()
+for key, value in ham_log_prob.items():
+    ham_log_prob[key] = math.log(value/(n_ham+v))
+
+# spam log probability dictionary
+spam_log_prob = spam_dict.copy()
+for key, value in spam_log_prob.items():
+    spam_log_prob[key] = math.log(value/(n_spam+v))
+
+# unseen ham log probability (probability of an unseen word being ham)
+unseen_ham_log_prob = math.log(alpha/(n_ham+(alpha*v)))
+
+# unseen spam log probability (probability of an unseen word being spam)
+unseen_spam_log_prob = math.log(alpha/(n_spam+(alpha*v)))
+
+# classifying the testing emails as ham or spam (they are classified so we know that they should be)
+# will check prob of each word being ham or spam, and then multiply all those (independent words)
+# if prob of the overall email being ham > prob of spam, then email is ham and vice versa
+# if the word is unseen, we use the above unseen word probabilities
+# the input is one of the testing file names, so we are pulling from that folder by default
+
+# classifying the emails in the given test file
+test_files = os.listdir("../HamSpam/test")
+test_email_classification = dict()
+for test_email in test_files:
+    ham_prob = 0
+    spam_prob = 0
+    with open(os.path.join("../HamSpam/test", test_email), 'r') as te:
+        lines = te.readlines()
+        for line in lines:
+            line = line.strip()
+            # if we see a stop word, we want to skip it (ignore when calc. probabilities)
+            if line.lower() in stop_words:
+                continue
+            # calculate ham log prob of this word
+            if line in ham_log_prob:
+                ham_prob = ham_prob + ham_log_prob[line]
+            else:
+                ham_prob = ham_prob + unseen_ham_log_prob
+            # calculate spam log prob of the same word
+            if line in spam_log_prob:
+                spam_prob = spam_prob + spam_log_prob[line]
+            else:
+                spam_prob = spam_prob + unseen_spam_log_prob
+
+    # check which prob is greater, ham or spam, and then that is the classification for this email
+    test_email = test_email.split(".", 1)[0]
+    if ham_prob >= spam_prob:
+        test_email_classification[test_email] = "HAM"
+    else:
+        test_email_classification[test_email] = "SPAM"
+
+
+# validating to see which emails in the test folder were classified correctly based on the truthfile
+# P = condition negative is a SPAM email (R) = # of file names in the truthfile
+# R = condition positive is a HAM email = # of test files (100) - # of files in the truthfile (R)
+# TP = SPAM email that got classified as SPAM
+# FN = SPAM email that got classified as HAM (incorrect)
+# FP = HAM email that got classified as SPAM (incorrect)
+# TN = HAM email that got classified as HAM
+# count = sanity check to make sure we count all 100 test emails
+P = 0
+R = 0
+TP = 0
+FN = 0
+FP = 0
+TN = 0
+count = 0
+with open("../HamSpam/truthfile", 'r') as tf:
+    spam_files = tf.readlines()
+    P = len(spam_files)
+    # removing all newlines from the elements in the list
+    spam_files = list(map(lambda s: s.strip(), spam_files))
+    for key in test_email_classification.keys():
+        count += 1
+        # if the key (file number) is in the spam_files list, it means that this email should have been classified as SPAM
+        if key in spam_files:
+            if test_email_classification[key] == "SPAM":
+                TP += 1
+            else:
+                FN += 1
+        # if the key (file num) is not in the spam_files list, then it means it should be classified as HAM
+        else:
+            if test_email_classification[key] == "HAM":
+                TN += 1
+            else:
+                FP += 1
+R = len(os.listdir("../HamSpam/test")) - P
+
+# calculating f-score
+# f-score = TP/(TP + 0.5(FP+FN))
+f_score = TP/(TP + 0.5*(FP+FN))
+
+# calculating precision --> quantifies num of positive class predictions that actually belong to the positive class
+# ie how many HAMS actually got classified as HAM
+# precision = TP/(TP+FP)
+precision = TP/(TP+FP)
+
+# calculating recall --> quantifies num of positive class predictions made out of all positive examples in the dataset
+# recall = TP/(TP+FN)
+recall = TP/(TP+FN)
+
+print("P = ", P)
+print("R = ", R)
+print("TP (SPAM classified as SPAM) = ", TP)
+print("FN (SPAM classified as HAM) = ", FN)
+print("FP (HAM classified as SPAM) = ", FP)
+print("TN (HAM classified as HAM) = ", TN)
+print("f-score = ", f_score)
+print("precision = ", precision)
+print("recall = ", recall)
+print("The TEST emails were classified as follows: ")
+test_key_list = sorted(list(map(int, test_email_classification.keys())))
+for key in test_key_list:
+    print(key, " = ", test_email_classification[str(key)])
+
+
+
+
+
+
+
